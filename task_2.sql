2890b639ca018157c3ac0ef8bf253676cb78f5bf
kidusom <kidusk2080@gmail.com>
2024-07-26 05:41:25 -0700

    modified:   task_2.sql

--- task_2.sql
+++ task_2.sql
@@ -24,8 +24,7 @@ CREATE TABLE Customers (
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
-    order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    order_date DATE NOT NULL, FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_details (
