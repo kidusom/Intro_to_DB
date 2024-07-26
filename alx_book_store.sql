bd6372e262fc691cb6a16869f88fa2ce973b15d3
kidusom <kidusk2080@gmail.com>
2024-07-26 04:31:52 -0700

    modified:   alx_book_store.sql

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,14 +1,11 @@
-CREATE DATABASE alx_book_store;
+CREATE DATABASE ALX_BOOK_STORE; 
+USE ALX_BOOK_STORE;
  
-USE alx_book_store;
-
--- Create the Authors table
 CREATE TABLE Authors (
     author_id INT AUTO_INCREMENT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
-
--- Create the Books table
+ 
 CREATE TABLE Books (
     book_id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
@@ -17,24 +14,21 @@ CREATE TABLE Books (
     publication_date DATE,
     FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
-
--- Create the Customers table
+ 
 CREATE TABLE Customers (
     customer_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL UNIQUE,
     address TEXT
 );
-
--- Create the Orders table
+ 
 CREATE TABLE Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
     FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
-
--- Create the Order_Details table
+ 
 CREATE TABLE Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
     order_id INT,
